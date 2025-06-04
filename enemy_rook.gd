7761c169dc834e4db60ceb37ddabcfcb7b5ccbd6
MiluLo <95948078+MiluLo@users.noreply.github.com>
2025-06-04 17:02:04 -0500

    initilization

---
+++ enemy_rook.gd
@@ -0,0 +1,72 @@
+extends CharacterBody2D
+
+const tile_size: Vector2 = Vector2(64,64)#64
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var astar_grid: AStarGrid2D
+@onready var tile_map: TileMapLayer = $"../TileMapLayer"
+var is_moving: bool
+
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+
+
+func _ready() -> void:
+	astar_grid = AStarGrid2D.new()
+	astar_grid.region = tile_map.get_used_rect()
+	astar_grid.cell_size = Vector2(64,64)
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_NEVER
+	astar_grid.update()
+	
+	var region_size = astar_grid.region.size
+	var region_position = astar_grid.region.position
+	
+	for x in region_size.x:
+		for y in region_size.y:
+			var tile_position = Vector2i(x + region_position.x,y + region_position.y)
+			var tile_data = tile_map.get_cell_tile_data(tile_position)
+			if tile_data == null or not tile_data.get_custom_data("walkable"):
+				astar_grid.set_point_solid(tile_position)
+
+func _process(_delta: float) -> void:
+	if is_moving:
+		return
+		
+	move()
+
+
+func move():
+	var path = astar_grid.get_id_path(
+		tile_map.local_to_map(global_position),
+		tile_map.local_to_map(player.global_position)
+		)
+	
+	path.pop_front()
+	
+	if path.size() == 1:
+		print("I have arrived good sir")
+		return
+	
+	if path.is_empty():
+		print("cant find path")
+		return
+
+	var original_position = Vector2(global_position)
+	
+	global_position = tile_map.map_to_local(path[0])
+	sprite.global_position = original_position
+	
+	is_moving = true
+	
+
+func _physics_process(_delta: float) -> void:
+	if is_moving:
+		sprite.global_position = sprite.global_position.move_toward(global_position,1)
+		
+		if sprite.global_position != global_position:
+			return
+		
+		is_moving = false
---
+++ scenes/rat_knight.gd
@@ -0,0 +1,51 @@
+extends CharacterBody2D
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -300.0
+var last_movement = Vector2.UP
+
+
+@onready var tile_map: TileMapLayer = $"../TileMapLayer"
+
+
+const tile_size: Vector2 = Vector2(64,64)#64
+var sprite_node_pos_tween: Tween
+
+func _physics_process(_delta: float) -> void:
+	if !sprite_node_pos_tween or !sprite_node_pos_tween.is_running():
+		if Input.is_action_just_pressed("up") and not $up.is_colliding():
+			move(Vector2.UP)
+			print("up")
+		if Input.is_action_just_pressed("down") and not $doown.is_colliding():
+			move(Vector2.DOWN)
+		if Input.is_action_just_pressed("left") and not $left.is_colliding():
+			move(Vector2.LEFT)
+			sprite.flip_h = true
+		if Input.is_action_just_pressed("right") and not $right.is_colliding():
+			move(Vector2.RIGHT)
+			sprite.flip_h = false
+
+func move(direction: Vector2):
+	#get current tile vector2i
+	var current_tile: Vector2 = tile_map.local_to_map(global_position)
+	#gettarget tile vector2i
+	var target_tile: Vector2i = Vector2i(
+		current_tile.x + direction.x, 
+		current_tile.y + direction.y
+		)
+	#get custom data layer from target tile
+	var tile_data: TileData = tile_map.get_cell_tile_data(target_tile)
+	
+	if tile_data == null or not tile_data.get_custom_data("walkable"):
+		return
+	
+	
+	global_position = tile_map.map_to_local(target_tile)
+
+	if sprite_node_pos_tween:
+		sprite_node_pos_tween.kill()
+	sprite_node_pos_tween = create_tween()
+	sprite_node_pos_tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
+	sprite_node_pos_tween.tween_property(sprite, "global_position", global_position, 0.15).set_trans(Tween.TRANS_SINE)
